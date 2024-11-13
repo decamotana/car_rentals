3b9ec4c8c7c92a2dd190a4d46efadd84ac669398
Jepril Achapero <jeprilachapero@gmail.com>
2024-11-14 01:01:39 +0800

    udpatre

---
+++ app/Models/Car.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function bookings()
+    {
+        return $this->hasMany(CarBooking::class);
+    }
+
+    public function attachments()
+    {
+        return $this->morphMany(Attachment::class, 'attachmentable');
+    }
+}
